7cbafb7e80261825497fbf2e557551f08bc94be8
Hotterbeekx <wiel.hotterbeekx@zuyd.nl>
2023-05-04 19:27:18 +0200

    voetballers

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):   
+class Voetbalspelers(models.Model):   
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    naam_speler = models.CharField(max_length=200)
+    actuele_voetbalclub = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
