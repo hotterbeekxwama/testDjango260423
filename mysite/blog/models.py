2044174c3aafba6bdac585b5f784d55cb2200e34
Hotterbeekx <wiel.hotterbeekx@zuyd.nl>
2023-05-05 11:25:33 +0200

    huiswerk les 4 opgave 1

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,15 +4,11 @@ from django.utils import timezone
 
 class Voetbalspelers(models.Model):   
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    naam_speler = models.CharField(max_length=200)
-    actuele_voetbalclub = models.TextField()
+    naam_voetballer = models.CharField(max_length=200)
+    actuele_voetbalclub = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
        self.published_date = timezone.now()
        self.save()
-
-    def __str__(self):
-       return self.title
-
