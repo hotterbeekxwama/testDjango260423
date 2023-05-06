328fa0ebf4090d2eae3264496a44692a4273c3d2
Hotterbeekx <wiel.hotterbeekx@zuyd.nl>
2023-05-06 15:06:21 +0200

    aanpassingen 5.6 Django: toevoegen untracked files

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Voetbalspelers
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Voetbalspelers.objects.filter(published_date=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # Create your views here.
